4586cc9f954090118fd5edd5333b43b4149c5d7b
snehil891 <snehilsah400@gmail.com>
2023-03-10 22:50:40 +0530

    Unecessary files removed

--- Functionality.js
+++ Functionality.js
@@ -1,4 +1,5 @@
-Changetoepic()
+function Changetoepic()
 {
     var x=document.getElementById("img1").src="./Images/assassin's.jpeg";
+    var y = document.getElementById("epic").style.boxShadow =rgba(0, 140, 186, 0.5);
 }
