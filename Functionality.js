2381985ce0b7150361f61ddf48332022f6b9199d
snehil891 <snehilsah400@gmail.com>
2023-03-11 19:18:16 +0530

    images added -- functionality onclick img change

--- Functionality.js
+++ Functionality.js
@@ -1,5 +1,68 @@
-function Changetoepic()
-{
-    var x=document.getElementById("img1").src="./Images/assassin's.jpeg";
-    var y = document.getElementById("epic").style.boxShadow =rgba(0, 140, 186, 0.5);
+function Changetoepic() {
+    document.getElementById("img1").src = "./Images/assassin's.jpeg";
+    document.getElementById("img2").src = "./Images/cyberpunk.webp";
+    document.getElementById("img3").src = "./Images/GTA5.jpeg";
+    document.getElementById("img4").src = "./Images/jedi.png";
+    document.getElementById("img5").src = "./Images/Readdead.jpg";
+    document.getElementById("img6").src = "./Images/ridersrepublic.jpg";
+    document.getElementById("epic").style.width = "90px";
+    document.getElementById("gog").style.width = "70px";
+    document.getElementById("steam").style.width = "70px";
+     
+}
+function Changetogog(){
+    document.getElementById("img1").src = "./Images/solasta.webp";
+    document.getElementById("img2").src = "./Images/steelrising.webp";
+    document.getElementById("img3").src = "./Images/teslaeffect.jpeg";
+    document.getElementById("img4").src = "./Images/thekillingmoon.jpeg";
+    document.getElementById("img5").src = "./Images/Witcher3.jpeg";
+    document.getElementById("img6").src = "./Images/wreckfest.jpeg";
+    document.getElementById("gog").style.width = "90px";
+    document.getElementById("epic").style.width = "70px";
+    document.getElementById("steam").style.width = "70px";        
+}
+function Changetosteam() {
+    document.getElementById("img1").src = "./Images/deadbydaylight.jpg";
+    document.getElementById("img2").src = "./Images/haloinfinite.jpg";
+    document.getElementById("img3").src = "./Images/deeprock.jpg";
+    document.getElementById("img4").src = "./Images/monsterhunter.jpg";
+    document.getElementById("img5").src = "./Images/total-war-3.jpg";
+    document.getElementById("img6").src = "./Images/nba-2k23.jpeg";
+    document.getElementById("steam").style.width = "90px";
+    document.getElementById("epic").style.width = "70px";
+    document.getElementById("gog").style.width = "70px";
+}
+
+
+
+function Changeepic(){
+    document.getElementById("epic").style.width = "100px";
+    document.getElementById("gog").style.width = "70px";
+    document.getElementById("steam").style.width = "70px";
+}
+function Changegog(){
+    document.getElementById("gog").style.width = "90px";
+    document.getElementById("epic").style.width = "70px";
+    document.getElementById("steam").style.width = "70px";
+    document.getElementById("one-img").src = "./More-images/gog/batman-arkhamasylum.jpeg";
+    document.getElementById("one-img").src = "./More-images/gog/empire-earth.jpeg";
+    document.getElementById("one-img").src = "./More-images/gog/Empire-Earth.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/heroes-5.jpeg";
+    document.getElementById("one-img").src = "./More-images/gog/heroes3.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/horizon.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/pathfinder.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/solasta.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/stalker-2.webp";
+    document.getElementById("one-img").src = "./More-images/gog/star-trek-armada.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/swat-4.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/System-Shock-2.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/terraformers.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/The-Last-Spell.webp";
+    document.getElementById("one-img").src = "./More-images/gog/world-in-conflict.jpg";
+    document.getElementById("one-img").src = "./More-images/gog/wreckfest.jpg";
+}
+function Changesteam(){
+    document.getElementById("steam").style.width = "90px";
+    document.getElementById("epic").style.width = "70px";
+    document.getElementById("gog").style.width = "70px";
 }
